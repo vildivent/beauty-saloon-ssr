cab55b31eb658920195fbd24293b9f2570689267
Romuald Brillout <vite-plugin-ssr@brillout.com>
2022-08-27 08:38:13 +0300

    boilerplate Vite w/ vite-plugin-ssr

---
+++ renderer/_default.page.client.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { PageShell } from './PageShell'
+
+export { render }
+
+async function render(pageContext) {
+  // We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
+  // instead of `getPage()`, see https://vite-plugin-ssr.com/useClientRouter
+  const { Page, pageProps } = pageContext
+  hydrateRoot(
+    document.getElementById('page-view'),
+    <PageShell pageContext={pageContext}>
+      <Page {...pageProps} />
+    </PageShell>
+  )
+}
+
+/* To enable Client-side Routing:
+export const clientRouting = true
+// !! WARNING !! Before doing so, read https://vite-plugin-ssr.com/clientRouting */
